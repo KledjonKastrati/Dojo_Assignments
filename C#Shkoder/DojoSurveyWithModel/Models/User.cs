441672aa0e1005c142faf4f293db7830567a41b7
KledjonKastrati <kastrati.kledjon@gmail.com>
2023-12-13 18:28:18 +0100

    DojoSurveyy

---
+++ C#Shkoder/DojoSurveyWithModel/Models/User.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+#pragma warning disable CS8618
+
+namespace DojoSurveyWithModel.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage ="emri duhet te plotesohet")]
+        [MinLength(4 , ErrorMessage ="gjatesia me e madhe se 3 shkronja")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Locationi duhet te plotesohet")]
+        [MinLength(2, ErrorMessage ="gjatesia me e madhe se 1 shkronj")]
+        public string Locations { get; set; }
+        [Required(ErrorMessage ="gjuha preferuar duhet te plotesohet")]
+        public string Languages { get; set; }
+        [MinLength(20 , ErrorMessage ="gjatesia me e madhe se 20 shkronja")]
+        public string? Comments { get; set; } 
+    }
+}
